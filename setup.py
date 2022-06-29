1996ca1741eb7168693bedb6ceba64817fcc25f9
Lei Chen <leichen88@yahoo.fr>
2022-06-29 17:10:18 +0200

    update package function

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup( name='unhcrpyplotstyle',
-       version='0.1',
+       version='0.0.1',
        author='Lei Chen',
        author_email="chen@unhcr.org",
-       description="Set matplotlib style to follow UNHCR's Data Visualization Guidelines",
+       description="Set matplotlib style following UNHCR's Data Visualization Guidelines",
        packages=['unhcrpyplotstyle'],
        install_requires=['matplotlib>=3.1.0'],
        include_package_data=True
