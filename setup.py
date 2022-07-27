4a5f1de2ba8df0ea63592ee6c367278186e2a9e6
Lei Chen <leichen88@yahoo.fr>
2022-07-27 16:12:51 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+# Get description from README
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup( name='unhcrpyplotstyle',
-       version='0.0.11',
+       version='0.1.2',
        author='Lei Chen',
        author_email="chen@unhcr.org",
        description="Set matplotlib style following UNHCR's Data Visualization Guidelines",
-       packages=['unhcrpyplotstyle'],
+       long_description = long_description,
+       long_description_content_type='text/markdown',
        license="MIT",
        keywords=[
         "matplotlib-style-sheets",
@@ -13,7 +18,12 @@ setup( name='unhcrpyplotstyle',
         "matplotlib-styles",
         "python"
        ],
+       classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+       ],
        url="https://github.com/leichen88/unhcrpyplotstyle",
-       install_requires=['matplotlib>=3.1.0'],
+       install_requires=['matplotlib'],
        include_package_data=True
-    )
+)
